7599ad8b95ae954c800d652fac06c7241b7b15f8
Grant Reighard <grant.reighard@gmail.com>
2022-06-16 22:11:14 -0400

    Uses env var for API_HOST. Fixes container height bug.

--- pages/[subdomain].js
+++ pages/[subdomain].js
@@ -86,7 +86,7 @@ function Site({ site, links }) {
 
 export async function getServerSideProps(context) {
     const { subdomain } = context.query;
-    const res = await fetch(`https://api.mostlink.co/api/sites/static/next/subdomain-${subdomain}`) || {};
+    const res = await fetch(`${process.env.API_HOST}/api/sites/static/next/subdomain-${subdomain}`) || {};
     const data = await res.json();
     const { site, links } = data;
   
