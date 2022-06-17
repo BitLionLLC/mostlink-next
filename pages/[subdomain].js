7a08af8b2dfae36ad8ffe3f49d5884c693af7439
Grant Reighard <grant.reighard@gmail.com>
2022-06-16 20:46:28 -0400

    Updates defaultHeader path.

--- pages/[subdomain].js
+++ pages/[subdomain].js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Head from "next/head";
 import { useEffect } from "react";
-import defaultHeader from '../../assets/default-header.png';
+import defaultHeader from '../assets/default-header.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
