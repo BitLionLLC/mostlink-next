bd2c01b7a9391a8b4a4bd2f50b70303c58413ebc
Grant Reighard <grant.reighard@gmail.com>
2026-04-05 14:22:23 -0400

    fix favicon

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,19 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import Head from 'next/head';
 
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 library.add(fab, far, fas);
